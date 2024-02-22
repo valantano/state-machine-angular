e817e63b854ce28e59a13e333238808c89cc799e
Valentino Geuenich <valentino.geuenich@rwth-aachen.de>
2024-02-22 12:03:27 +0100

    Bezier curves and line segments working + line hover working

--- src/app/behavior-tree/transition-edge/transition-edge.component.ts
+++ src/app/behavior-tree/transition-edge/transition-edge.component.ts
@@ -3,7 +3,9 @@ import { Component, Input, CUSTOM_ELEMENTS_SCHEMA, HostListener} from '@angular/
 @Component({
   selector: 'app-transition-edge',
   template: `<svg>
-  <line [attr.x1]="startX" [attr.y1]="startY" [attr.x2]="endX" [attr.y2]="endY" />
+  <!-- <line [attr.x1]="startX" [attr.y1]="startY" [attr.x2]="endX" [attr.y2]="endY" /> -->
+  <path [attr.d]="getPath()" stroke="black" fill="transparent" stroke-linejoin="round" stroke-linecap="round"/>
+
 </svg>`,
   styleUrl: './transition-edge.component.scss'
 })
@@ -15,6 +17,32 @@ export class TransitionEdgeComponent {
 
   @Input() sourceId: number = -1;
   @Input() targetId: number = -1;
+
+  getPath(): string {
+
+    
+    if (this.startY > this.endY) {
+      const belowStartY = this.startY + 50;
+      const aboveEndY = this.endY - 50;
+      let besideX;
+      if (this.startX > this.endX) {
+      
+        besideX = Math.max(this.startX + 200, this.endX + 200);
+      } else {
+        besideX = Math.min(this.startX - 200, this.endX - 200);
+      }
+
+      return `M ${this.startX} ${this.startY} L ${this.startX} ${belowStartY} L ${besideX} ${belowStartY} L ${besideX} ${aboveEndY} L ${this.endX} ${aboveEndY} L ${this.endX} ${this.endY}`;
+    } else {
+      const controlPointStartX = this.startX;
+      const controlPointEndX = this.endX;
+      const controlPointStartY = this.startY + 100;
+      const controlPointEndY = this.endY - 100;
+      return `M ${this.startX} ${this.startY} C ${controlPointStartX} ${controlPointStartY} ${controlPointEndX} ${controlPointEndY} ${this.endX} ${this.endY}`;
+
+    }
+  
+  }
 
 
 
